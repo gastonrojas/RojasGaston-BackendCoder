d40904ab8ad471b123395dd8680ae738f9b87c7f
tonga <grojas89@hotmail.com>
2022-03-17 17:07:14 -0300

    created class Usuario in main.ja, created index.html

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
+class Usuario {
+  constructor(nombre, apellido, libros, mascotas) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.libros = libros;
+    this.mascotas = mascotas;
+  }
 
-``
+  getFullName() {
+    return `${this.nombre} ${this.apellido}`;
+  }
+  addMascota(nuevaMascota) {
+    this.mascotas.push(nuevaMascota);
+  }
+  countMascotas() {
+    return this.mascotas.length;
+  }
+  addBook(nombre, autor) {
+    this.libros.push({ nombre: nombre, autor: autor });
+  }
+  getBooks() {
+    const bookNames = [];
+    for (let i = 0; i < this.libros.length; i++) {
+      bookNames.push(this.libros[i].nombre);
+    }
+    return bookNames;
+  }
+}
